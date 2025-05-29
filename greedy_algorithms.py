cfc10776a59b0142020fedbab120dd654ea687a6
rajhrajesh <penurajeshitsd@gmail.com>
2025-05-30 01:00:00 +0530

    greedy algorithm added

---
+++ greedy_algorithms.py
@@ -0,0 +1,26 @@
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+stations = {}
+stations["knone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+
+final_stations = set()
+
+while states_needed:
+    best_station = None
+    states_covered = set()
+    for station, states_for_station in stations.items():
+        # Find which states this station covers that are still needed
+        covered = states_needed & states_for_station
+        if len(covered) > len(states_covered):
+            best_station = station
+            states_covered = covered
+
+    # Remove the states covered by the chosen station from the states needed
+    states_needed -= states_covered
+    final_stations.add(best_station)
+
+print(final_stations)
--- recurive_sum.py
+++ recurive_sum.py
@@ -1,25 +1,32 @@
-def recursive_sum(arr):
-    if len(arr) == 0:
-        return 0 
-    return arr[0] + recursive_sum(arr[1:])
+# def recursive_sum(arr):
+#     if len(arr) == 0:
+#         return 0 
+#     return arr[0] + recursive_sum(arr[1:])
 
-numbers = [2,4,6,11]
-print(recursive_sum(numbers))
+# numbers = [2,4,6,11]
+# print(recursive_sum(numbers))
 
-# 2 programm'
-def recursive_count(arr):
-    if len(arr) == 0:
-        return 0 
-    return 1 + recursive_count(arr[1:])
+# # 2 programm'
+# def recursive_count(arr):
+#     if len(arr) == 0:
+#         return 0 
+#     return 1 + recursive_count(arr[1:])
 
-numbers = [12,3,4,5,6]
-print(recursive_count(numbers))
+# numbers = [12,3,4,5,6]
+# print(recursive_count(numbers))
 
-# 3 programm' 
-def recursive_max(arr):
-    if len(arr) == 1:
-        return arr[0]
-    max_rest = recursive_max(arr[1:])
-    return arr[0] if arr[0] > max_rest else max_rest 
-arr = [12,3,4,5,6]
-print(recursive_max(arr))
+# # 3 programm' 
+# def recursive_max(arr):
+#     if len(arr) == 1:
+#         return arr[0]
+#     max_rest = recursive_max(arr[1:])
+#     return arr[0] if arr[0] > max_rest else max_rest 
+# arr = [12,3,4,5,6]
+# print(recursive_max(arr))
+
+def max(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
+    sub_max = max(list[1:])
+    return list[0] if list[0] > sub_max else sub_max
+print(max([1,2,4,5]))
